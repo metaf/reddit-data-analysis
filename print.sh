0d8031bf913b47eaf4b4462a693147c8376d2f02
metaf <taf34@pitt.edu>
2016-02-15 02:19:50 -0500

    add some escaped quotes

--- print.sh
+++ print.sh
@@ -2,7 +2,7 @@
 MY_DIR=$(dirname $(readlink -f $0))
 DATADIR=/mnt/tferrell/reddit_data/2007/
 WORKDIR=/mnt/tferrell/scratch
-map="python /mnt/tferrell/reddit-data-analysis/printComments.py $WORKDIR"
+map="\"python /mnt/tferrell/reddit-data-analysis/printComments.py $WORKDIR\""
 #This map will create a file in the workdir for each input file.
 #it simply pretty prints the comment data
 
