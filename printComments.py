6c372eef4ee50630e6b441068abba21ca7ac764d
metaf <taf34@pitt.edu>
2016-02-17 01:21:12 -0500

    different pprint syntax

--- printComments.py
+++ printComments.py
@@ -2,9 +2,9 @@ import os
 import os.path
 import base
 import pprint
-pp=pprint.PrettyPrinter(indent=4)
+#pp=pprint.PrettyPrinter(indent=4)
 
 with open(base.OUTPUT_FILE,"w") as f:
     for comment in base.getComments():
-        pp.pprint(comment,stream=f)
+        pprint.pprint(comment,stream=f)
         #f.write(str(comment))
