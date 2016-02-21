1d047e3e2a0f92a350e9b935be1465767c8ce7d2
metaf <taf34@pitt.edu>
2016-02-20 20:19:34 -0500

    count flaired scores vs non-flaired scores

--- countFlairedVSTotal.py
+++ countFlairedVSTotal.py
@@ -1,1 +1,36 @@
+import os
+import os.path
 import base
+
+#This map will create a file in the workdir for each input file.
+#The file will have 3 lines:
+#-total comments
+#-num with flair
+#-num without flair
+#-avg_flaired_score
+#-avg_noflaired_score
+
+
+noflair_average = 0
+noflair_count = 0
+
+flair_average = 0
+flair_count = 0
+for comment in base.getComments():
+    flairtext = comment["author_flair_text"]
+    if flairtext is None:
+        noflair_count += 1
+        noflair_average = noflair_average * \
+            (noflair_count - 1) / noflair_count + comment["score"]
+    else:
+        flair_count += 1
+        flair_average = flair_average * \
+            (flair_count - 1) / flair_count + comment["score"]
+
+
+with open(base.OUTPUT_FILE, "w") as f:
+    f.write(str(noflair_count + flair_count))
+    f.write(str(flair_count))
+    f.write(str(noflair_count))
+    f.write(str(flair_average))
+    f.write(str(noflair_average))
--- flair.sh
+++ flair.sh
@@ -6,8 +6,10 @@ map="python /mnt/tferrell/reddit-data-analysis/countFlairedVSTotal.py $WORKDIR"
 #This map will create a file in the workdir for each input file.
 #The file will have 3 lines:
 #-total comments
-#-comments with flair
-#-percent with flair
+#-num with flair
+#-num without flair
+#-avg_flaired_score
+#-avg_noflaired_score
 
 
 reduce(){
